10e51df7fc4d6132807067646b38c6eace0a97c7
AntonAA <AntonAAMain@yandex.ru>
2024-09-12 16:20:35 +0300

    added hovering to call-order

--- src/components/layout/Header/CallOrder/CallOrder.tsx
+++ src/components/layout/Header/CallOrder/CallOrder.tsx
@@ -4,13 +4,26 @@ import { useCallModalStore } from "@/shared/zustand/useCallModalStore";
 import cn from "classnames";
 
 import cls from "./CallOrder.module.scss";
+import { useState } from "react";
 
 export const CallOrder = () => {
   const { open } = useCallModalStore();
 
+  const [isTextActive, setIsTextActive] = useState<boolean>(false);
+
+  const toggle = () => setIsTextActive((prev) => !prev);
+
   return (
-    <div onClick={open} className={cn(cls.container)}>
-      Заказать звонок
+    <div
+      onMouseEnter={toggle}
+      onMouseLeave={toggle}
+      onClick={open}
+      className={cn(cls.container)}
+    >
+      <div className={cn(cls.text, { [cls.text_active]: isTextActive })}>
+        <p>Заказать звонок</p>
+        <p>Заказать звонок</p>
+      </div>
     </div>
   );
 };
